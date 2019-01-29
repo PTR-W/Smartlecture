2d54d0caaa6041f8b2385c7b37bc682609f6e9b9
Abrahalhabachi <33718367+Abrahalhabachi@users.noreply.github.com>
2019-01-29 16:56:57 +0100

    open_camera added

---
+++ app/src/main/java/de/SmartLecture/application/activity/OpenCamera.java
@@ -0,0 +1,138 @@
+package com.example.iliass.smartlecture;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.content.FileProvider;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URI;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import android.app.Activity;
+import android.graphics.PixelFormat;
+import android.hardware.Camera;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+public class OpenCamera extends AppCompatActivity {
+
+    public static final String LOG_TAG = "MyLogTag";
+    private static final int CAMERA_REQUEST = 1;
+    private static final int PERMISSION_REQUEST = 2;
+    private String imageFilePath = "";
+    private Button CameraBtn;
+    private Button ScheduleBtn;
+    private ImageView image;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.open_camera);
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) !=
+                PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                    PERMISSION_REQUEST);
+        }
+        Log.i(LOG_TAG, "created");
+        //CamBtnListener ();
+        //ScheduleBtnListener();
+    }
+
+    private void ScheduleBtnListener() {
+        ScheduleBtn = findViewById(R.id.btnSchedule);
+        ScheduleBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //startActivity(new Intent(//.this, schedule.class));
+            }
+        });
+    }
+
+
+    private void CamBtnListener() {
+        CameraBtn = findViewById(R.id.btnCamera);
+        CameraBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openCameraIntent();
+            }
+        });
+    }
+
+    private void openCameraIntent() {
+        Intent pictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (pictureIntent.resolveActivity(getPackageManager()) != null) {
+
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            }
+            catch (IOException e) {
+                e.printStackTrace();
+                return;
+            }
+            Uri photoUri = FileProvider.getUriForFile(this, "com.example.iliass.smartlecture.provider", photoFile);
+            pictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoUri);
+            startActivityForResult(pictureIntent, CAMERA_REQUEST);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (requestCode == PERMISSION_REQUEST && grantResults.length > 0) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(this, "Thanks for granting Permission", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    private File createImageFile() throws IOException{
+
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss", Locale.getDefault()).format(new Date());
+        String imageFileName = "IMG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(imageFileName, ".jpg", storageDir);
+        imageFilePath = image.getAbsolutePath();
+
+        return image;
+    }
+
+    protected void onResume () {
+        super.onResume();
+        setContentView(R.layout.open_camera);
+        Log.i(LOG_TAG, "resumed");
+        image= findViewById(R.id.image);
+        image.setImageURI(Uri.parse(imageFilePath));
+        CamBtnListener();
+        ScheduleBtnListener();
+    }
+
+    protected void onActivityResult ( int requestCode, int resultCode, Intent data){
+        if (requestCode == 1 && resultCode == RESULT_OK) {
+            image.setImageURI(Uri.parse(imageFilePath));
+        }
+    }
+}
