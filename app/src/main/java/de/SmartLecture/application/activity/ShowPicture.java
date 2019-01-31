2af82ed05acd36e966261a7a44c9b9d4d4bf2d1e
Abrahalhabachi <33718367+Abrahalhabachi@users.noreply.github.com>
2019-01-31 16:58:21 +0100

    Small Tweak
    fileName generatied from trimming the temp file name

--- app/src/main/java/de/SmartLecture/application/activity/ShowPicture.java
+++ app/src/main/java/de/SmartLecture/application/activity/ShowPicture.java
@@ -14,10 +14,6 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.Locale;
-
 import de.SmartLecture.R;
 
 import static android.os.Environment.DIRECTORY_PICTURES;
@@ -50,8 +46,10 @@ public class ShowPicture extends AppCompatActivity {
 
             File dir = new File(root.getAbsolutePath() + "/SmartLecture");
             dir.mkdirs();
-            String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss", Locale.getDefault()).format(new Date());
-            String imageFileName = "IMG_" + timeStamp + "_";
+            int trimStart = filePath.lastIndexOf("/");
+            int trimEnd = filePath.lastIndexOf(".");
+            String imageFileName= filePath.substring(trimStart, trimEnd);
+            Log.i(LOG_TAG, imageFileName);
             File file = new File(root.getAbsolutePath() + "/SmartLecture/"+imageFileName+".jpg");
 
             try
