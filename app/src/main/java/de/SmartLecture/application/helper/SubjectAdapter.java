378c25357de7d8482429c0d95da1f9318b0b2c6e
Abrahalhabachi <33718367+Abrahalhabachi@users.noreply.github.com>
2019-02-05 14:24:26 +0100

    Test ListAdapter

--- app/src/main/java/de/SmartLecture/application/helper/SubjectAdapter.java
+++ app/src/main/java/de/SmartLecture/application/helper/SubjectAdapter.java
@@ -1,7 +1,8 @@
 package de.SmartLecture.application.helper;
 
-import android.net.sip.SipSession;
 import android.support.annotation.NonNull;
+import android.support.v7.recyclerview.extensions.ListAdapter;
+import android.support.v7.util.DiffUtil;
 import android.support.v7.widget.RecyclerView;
 import android.view.ContextMenu;
 import android.view.LayoutInflater;
@@ -14,9 +15,28 @@ import java.util.List;
 
 import de.SmartLecture.R;
 
-public class SubjectAdapter extends RecyclerView.Adapter<SubjectAdapter.SubjectHolder> {
+//public class SubjectAdapter extends RecyclerView.Adapter<SubjectAdapter.SubjectHolder> {
+public class SubjectAdapter extends ListAdapter<Subject, SubjectAdapter.SubjectHolder> {
 
-    private List<Subject> subjects = new ArrayList<>();
+    //private List<Subject> subjects = new ArrayList<>();
+    private OnItemClickListener listener;
+
+    public SubjectAdapter() {
+        super(DIFF_CALLBACK);
+    }
+    private static final DiffUtil.ItemCallback<Subject> DIFF_CALLBACK = new DiffUtil.ItemCallback<Subject>() {
+        @Override
+        public boolean areItemsTheSame(Subject oldItem, Subject newItem) {
+            return oldItem.getId() == newItem.getId();
+        }
+
+        @Override
+        public boolean areContentsTheSame(Subject oldItem, Subject newItem) {
+            return oldItem.getTitle().equals(newItem.getTitle())
+                && oldItem.getDateStart().equals(newItem.getDateStart())
+                && oldItem.getDateEnd().equals(newItem.getDateEnd());
+        }
+    };
 
     @NonNull
     @Override
@@ -27,30 +47,33 @@ public class SubjectAdapter extends RecyclerView.Adapter<SubjectAdapter.SubjectH
 
     @Override
     public void onBindViewHolder(@NonNull final SubjectHolder holder, int position) {
-        Subject currentSubject = subjects.get(position);
+        //Subject currentSubject = subjects.get(position);
+        Subject currentSubject = getItem(position);
         holder.textViewTitle.setText(currentSubject.getTitle());
         holder.textViewDateEnd.setText(currentSubject.getDateEnd());
         holder.textViewDateStart.setText(currentSubject.getDateStart());
     }
 
-    @Override
-    public int getItemCount() {
-        return subjects.size();
-    }
-
-    public void setSubjects(List<Subject> subjects) {
-        this.subjects = subjects;
-        notifyDataSetChanged();
-    }
+//    @Override
+//    public int getItemCount() {
+//        return subjects.size();
+//    }
+//
+//    public void setSubjects(List<Subject> subjects) {
+//        this.subjects = subjects;
+//        notifyDataSetChanged();
+//    }
 
     public Subject getSubjectAt(int position) {
-        return subjects.get(position);
+//        return subjects.get(position);
+        return getItem(position);
     }
 
-    public class SubjectHolder extends RecyclerView.ViewHolder implements View.OnCreateContextMenuListener {
+    public class SubjectHolder extends RecyclerView.ViewHolder implements View.OnCreateContextMenuListener, View.OnClickListener {
         private TextView textViewTitle;
         private TextView textViewDateStart;
         private TextView textViewDateEnd;
+
 
         public SubjectHolder(View itemView) {
             super(itemView);
@@ -58,6 +81,7 @@ public class SubjectAdapter extends RecyclerView.Adapter<SubjectAdapter.SubjectH
             textViewDateStart = itemView.findViewById(R.id.text_view_date_start);
             textViewDateEnd = itemView.findViewById(R.id.text_view_date_end);
             itemView.setOnCreateContextMenuListener(this);
+            itemView.setOnClickListener(this);
         }
 
         @Override
@@ -65,5 +89,23 @@ public class SubjectAdapter extends RecyclerView.Adapter<SubjectAdapter.SubjectH
             menu.add(this.getAdapterPosition(), R.id.option_edit,   0,"Edit");
             menu.add(this.getAdapterPosition(), R.id.option_delete, 0,"Delete");
         }
+
+        //itemView.setOnClickListener(new View.OnClickListener(){
+        @Override
+        public void onClick(View v) {
+            if (listener != null)
+            {
+                listener.OnItemClick(getItem(getAdapterPosition()));
+//                listener.OnItemClick(subjects.get(getAdapterPosition()));
+            }
+        }
+    }
+
+    public interface OnItemClickListener{
+        void OnItemClick(Subject subject);
+    }
+
+    public void setOnItemClickListener(OnItemClickListener listener){
+        this.listener = listener;
     }
 }
